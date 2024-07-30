10fef57302850a0da908b7d7a1d600598c43d7a4
Nicolas Castania <nicocasta.dev@gmail.com>
2024-07-30 10:33:28 +0200

    + ligne vide au dessus

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 
 def tableau_vide(ligne, colonne):
+    print('  ' * colonne)
+
+
     for _ in range(ligne):
         for _ in range(colonne):
             print('⚫', end=' ')
         print() 
+
+
 
 ligne = 6
 colonne = 7
