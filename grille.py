99c6006ddbde880f54ff693d5530fd19ed158d28
FloGob42 <florent.gobin@gmail.com>
2024-07-30 13:40:22 +0200

    ajout de def victoire (pas 100% fonctionnel, ne sort pas de la boucle)

--- grille.py
+++ grille.py
@@ -3,6 +3,7 @@ largeur = 7
 joueur1 ="❌"
 joueur2= "💩"
 plateau = [['🔘' for _ in range(largeur)] for _ in range(hauteur)]
+termine = False
 
 def afficher_grille():
     for h in range(hauteur):
@@ -20,14 +21,34 @@ def placer_pion(col, joueur):
             plateau[h][col] = joueur
             break
     afficher_grille()
+                
+
+
+def victoire():
+    global termine
+    for h in range(hauteur):
+        for l in range(largeur):
+            if ((h<=2 and plateau[h][l] == joueur1) and (plateau[h+1][l] == plateau[h+2][l] == plateau[h+3][l] == joueur1)) or ((l<=3 and plateau[h][l] == joueur1 ) and (plateau[h][l+1] == plateau[h][l+2] == plateau[h][l+3] == joueur1)) or ((h<=2 and l<=3 and plateau[h][l] == joueur1) and (plateau[h+1][l+1] == plateau[h+2][l+2] == plateau[h+3][l+3] == joueur1)) or ((h<=2 and l>=3 and plateau[h][l] == joueur1) and (plateau[h+1][l-1] == plateau[h+2][l-2] == plateau[h+3][l-3] == joueur1)):
+                print("Puissance 4! La victoire revient au joueur 1!")
+                termine = True
+
+            elif ((h<=2 and plateau[h][l] == joueur2) and (plateau[h+1][l] == plateau[h+2][l] == plateau[h+3][l] == joueur2)) or ((l<=3 and plateau[h][l] == joueur2 ) and (plateau[h][l+1] == plateau[h][l+2] == plateau[h][l+3] == joueur2)) or ((h<=2 and l<=3 and plateau[h][l] == joueur2) and (plateau[h+1][l+1] == plateau[h+2][l+2] == plateau[h+3][l+3] == joueur2)) or ((h<=2 and l>=3 and plateau[h][l] == joueur2) and (plateau[h+1][l-1] == plateau[h+2][l-2] == plateau[h+3][l-3] == joueur2)):
+                print("Puissance 4! La victoire revient au joueur 2!")
+                termine = True
+    
 
 
 def jouer():
-    while True:
+    while termine == False:
         col = int(input("joueur 1, dans quelle colonne voulez-vous placer votre pion?"))
         placer_pion(col, joueur1)
+        victoire()
         col = int(input("joueur 2, dans quelle colonne voulez-vous placer votre pion?"))
         placer_pion(col, joueur2)
+        victoire()
+
+
+
 
 afficher_grille()
 jouer()
