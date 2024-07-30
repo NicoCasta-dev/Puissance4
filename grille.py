9b7bff991f0354e5311b57f28aacdebfe3f5f976
FloGob42 <florent.gobin@gmail.com>
2024-07-30 10:13:39 +0200

    grille.py

---
+++ grille.py
@@ -0,0 +1,10 @@
+hauteur =6
+largeur = 7
+
+plateau = [['Ο' for _ in range(largeur)] for _ in range(hauteur)]
+
+for h in range(hauteur):
+    for l in range(largeur):
+        print((plateau[h][l]), end = "    ")
+    print("\n")
+
